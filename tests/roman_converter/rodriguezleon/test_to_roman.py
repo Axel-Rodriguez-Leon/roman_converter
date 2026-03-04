43a0b30fdeaa349733035982f7dbd514830f044b
Axel <axelarl@udel.edu>
2026-03-04 17:19:05 -0500

    Fixed tests
    Changed the names to actually match the fuction names

--- tests/roman_converter/rodriguezleon/test_to_roman.py
+++ tests/roman_converter/rodriguezleon/test_to_roman.py
@@ -1,9 +1,9 @@
 from types import ModuleType
 
-def test_to_roman(impl: ModuleType):
-    assert impl.to_roman(1) == "I"
-    assert impl.to_roman(4) == "IV"
-    assert impl.to_roman(9) == "IX"
-    assert impl.to_roman(58) == "LVIII"
-    assert impl.to_roman(1994) == "MCMXCIV"
-    assert impl.to_roman(3999) == "MMMCMXCIX"
+def test_arabic_to_roman(impl: ModuleType):
+    assert impl.arabic_to_roman(1) == "I"
+    assert impl.arabic_to_roman(4) == "IV"
+    assert impl.arabic_to_roman(9) == "IX"
+    assert impl.arabic_to_roman(58) == "LVIII"
+    assert impl.arabic_to_roman(1994) == "MCMXCIV"
+    assert impl.arabic_to_roman(3999) == "MMMCMXCIX"
